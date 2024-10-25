1b657002e2067ae9efc8b1a0c595063040997a3f
hlz <2337991899@qq.com>
2024-10-25 11:16:59 +0800

    添加忽略插件配置文件

---
+++ crm/target/classes/public/js/user/add.update.js
@@ -0,0 +1,56 @@
+layui.use(['form', 'layer','formSelects'], function () {
+    var form = layui.form,
+        layer = parent.layer === undefined ? layui.layer : top.layer,
+        $ = layui.jquery;
+    var  formSelects = layui.formSelects;/*接收formSelects，下拉框*/
+
+
+    /**
+     * 设置下拉框
+     */
+    var userId=$("input[name='id']").val();
+    formSelects.config('selectId',{
+        type:"post",
+        searchUrl:ctx+"/role/queryAllRoles?userId="+userId,
+        //自定义返回数据中name的key, 默认 name
+        keyName: 'roleName',
+        //自定义返回数据中value的key, 默认 value
+        keyVal: 'id'
+    },true);
+
+
+    form.on('submit(addOrUpdateUser)',function (data) {
+        var index= top.layer.msg("数据提交中,请稍后...",{icon:16,time:false,shade:0.8});
+        var url = ctx+"/user/add";
+
+        //判断用户ID是否为空，如果不为空则为更新操作
+        if($("input[name='id']").val()){
+            url=ctx+"/user/update";
+        }
+        console.log(data.field);
+        $.post(url,data.field,function (res) {
+            if(res.code==200){
+                top.layer.msg("操作成功",{icon:6});
+                top.layer.close(index);
+                layer.closeAll("iframe");
+                // 刷新父页面
+                parent.location.reload();
+            }else{
+                layer.msg(res.msg,{icon:5});
+            }
+        });
+        return false;
+    });
+
+    /**
+     * 关闭弹出层
+     */
+    $("#closeBtn").click(function () {
+        // 先得到当前iframe层的索引
+        var index = parent.layer.getFrameIndex(window.name);
+        // 再执⾏关闭
+        parent.layer.close(index);
+    });
+
+
+});
